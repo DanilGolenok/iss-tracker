eafbdd6116dee1c3fd019a1cc220b3b6ee4df020
DanilGolenok <golenok.danil456@gmail.com>
2020-01-19 06:24:39 +0200

    Updates

---
+++ precache-manifest.3cd2b47cb160d6fcb6b5a1905d99a321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59cc8ebb1b885905c50d97aeac5656d",
+    "url": "/iss-tracker/index.html"
+  },
+  {
+    "revision": "2777fe580b24dc5c3631",
+    "url": "/iss-tracker/static/css/main.35cdfc07.chunk.css"
+  },
+  {
+    "revision": "75aae81e0d26d7e44b81",
+    "url": "/iss-tracker/static/js/2.2c60d387.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/iss-tracker/static/js/2.2c60d387.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2777fe580b24dc5c3631",
+    "url": "/iss-tracker/static/js/main.2aeb8eb3.chunk.js"
+  },
+  {
+    "revision": "f5047fc3e520d368c883",
+    "url": "/iss-tracker/static/js/runtime-main.1c26a9d2.js"
+  }
+]);
